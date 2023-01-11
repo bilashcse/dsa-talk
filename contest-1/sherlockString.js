1165804baa6d981238f030c90c8e115c9eca3efe
bilashcse <bilashcse@gmail.com>
2023-01-12 00:25:00 +0600

    sherlockString logic improvements

--- contest-1/sherlockString.js
+++ contest-1/sherlockString.js
@@ -1,25 +1,34 @@
 const testCase = [`aabbccddeefghi`, 'abcdefghhgfedecba'];
 
 function isValid(s) {
-	let isValid = true;
+
 	const stringMap = new Map();
 	for(let i =0; i< s.length; i++) {
 		const currentChar = s[i];
 		const currentCharCount = stringMap.get(currentChar) || 0;
 		stringMap.set(currentChar, currentCharCount + 1);
 	}
-	
 	const countMap = new Map();
+	
 	for (const [key, value] of stringMap.entries()) {
 		const oldValue = countMap.get(value) || '';
 		const currentValue = `${oldValue}${key}`;
 		countMap.set(value, currentValue);
 	}
 	
+	if(countMap.size === 1) {
+		return 'YES';
+	}
+	if(countMap.size > 2) {
+		return 'NO';
+	}
 	
-	console.log(countMap.entries())
-	return isValid;
 	
+	
+	console.log(countMap)
+	//TODO: need to implement logic for size 2 only
+//	return 
+		
 }
 
-console.log(isValid(testCase[1]));
+console.log(isValid(testCase[0]));
